1ea8a8fbf7fc6828f247558f67705d36a726598a
Kivilun <vkonoplev03@mail.ru>
2025-06-30 11:46:53 +0300

    Refine Stavropol news theme and improve styling

--- Kivilun/main/views.py
+++ Kivilun/main/views.py
@@ -2,13 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     data = {
-        'title': 'Главная страница',
-        'values': ['Some', 'Hello', '123'],
-        'obj': {
-            'car': 'BMW',
-            'age': '18',
-            'hobby': 'Football'
-        }
+        'title': 'Ставропольские новости',
     }
 
     return render(request, 'main/index.html', data)
